73949bc33d7dd57ca94bb8e710e1292bf983a309
Westley Knight <westley.knight1@justice.gov.uk>
2025-05-21 11:15:14 +0100

    Entry to service up to profile screens (#4)

--- app/_old_routes.js
+++ app/_old_routes.js
@@ -7,5 +7,3 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
-
-require('./routes/v2/routes.js')(router);
---
+++ app/routes/v2/routes.js
@@ -0,0 +1,12 @@
+var version = "/v2/";
+
+module.exports = function(router) {
+
+/**
+  router.post(version + 'url', function (req, res) {
+    if (req.session.data['var_name'] == "Yes")
+  {
+*/
+  module.exports = router
+
+}
